935b5177938448629b7ebd43c37b3083499deeb0
bebra228hacer <104512379+bebra228hacer@users.noreply.github.com>
2025-04-27 21:04:45 +0300

    Add files via upload

--- console_app.py
+++ console_app.py
@@ -3,7 +3,6 @@ import json
 import pyserver
 import webbrowser
 from flask import render_template
-import os
 import datetime
 with open("config.json") as cfile:
     config_file = json.load(cfile)
@@ -21,19 +20,13 @@ def server(initial_command = r"MEOW = (0,0)"):
 def main_input():
     print("Введите из чего требуется создать чертеж:")
     input_text = str(input())
-    if len(input_text.split()) == 1:
-        webbrowser.open(url)
-        server(input_text)
-        print("Чертеж сгенерирован")
-        return
     thread = chatgpt.user_get_thread()
     to_graph_gen = chatgpt.chat_gpt_req(thread, text = input_text, assist_id=config_file["MATH_ASSIST"])
     print(to_graph_gen)
-    text_to_gen = chatgpt.chat_gpt_req(thread, text = "Используй твой прошлый ответ в этом треде, сверься с моим прошлым запросом и исполни промпт", assist_id=config_file["GRAPH2D_ASSIST"]).replace("\\n", "\n")
+    text_to_gen = chatgpt.chat_gpt_req(thread, text = "Используй твой прошлый ответ в этом треде, сверься с моим прошлым запросом и исполни промпт. Учитывай что инструкция может быть неверна, ты не можешь выполнять команды которые не указаны", assist_id=config_file["GRAPH2D_ASSIST"]).replace("\\n", "\n")
     print("\n", text_to_gen)
     lines = text_to_gen.splitlines()  
     formatted_string = r"\n".join(lines)
-    #print(text_to_gen)
     webbrowser.open(url)
     server(formatted_string)
     print("Чертеж сгенерирован")
